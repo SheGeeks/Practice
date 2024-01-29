2fbff7e229cae4040fa36da16b538cfbccc4a5a6
Corvida Raven <dev@shegeeks.net>
2024-01-29 13:37:49 -0500

    And so it begins...

---
+++ ict-landing1/script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  grid: {
+    rows: 2,
+  },
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
